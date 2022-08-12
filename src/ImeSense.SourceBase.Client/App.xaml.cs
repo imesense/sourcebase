3e5e88c1fa9e900367f1e98378bccc004694d96f
acidicMercury8 <acidicmercury8@outlook.com>
2022-08-12 18:15:26 +0300

    Redefine entry point

--- src/ImeSense.SourceBase.Client/App.xaml.cs
+++ src/ImeSense.SourceBase.Client/App.xaml.cs
@@ -6,4 +6,10 @@ namespace ImeSense.SourceBase.Client;
 /// Interaction logic for App.xaml
 /// </summary>
 public partial class App : Application {
+    protected override void OnStartup(StartupEventArgs e) {
+        base.OnStartup(e);
+
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
 }
