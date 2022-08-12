6f9dad2bc1695ba8a63a2074b6b6366b90f05c48
acidicMercury8 <acidicmercury8@outlook.com>
2022-08-12 18:29:34 +0300

    Implement exit command

--- src/ImeSense.SourceBase.Client/ViewModels/MainViewModel.cs
+++ src/ImeSense.SourceBase.Client/ViewModels/MainViewModel.cs
@@ -1,4 +1,10 @@
+using System.Windows;
+
 namespace ImeSense.SourceBase.Client.ViewModels;
 
 public class MainViewModel : BaseViewModel {
+    private RelayCommand? _exitMenuItemCommand;
+    public RelayCommand ExitMenuItemCommand => _exitMenuItemCommand ??= new RelayCommand(_ => {
+        Application.Current.Shutdown();
+    });
 }
