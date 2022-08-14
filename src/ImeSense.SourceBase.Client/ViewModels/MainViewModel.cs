bb8d77dde113c685538c4e8fb959b9b5c0fde906
Dello961 <dello931@yandex.ru>
2022-08-14 19:14:48 +0700

    Add `SettingsWindow` with view model

--- src/ImeSense.SourceBase.Client/ViewModels/MainViewModel.cs
+++ src/ImeSense.SourceBase.Client/ViewModels/MainViewModel.cs
@@ -1,10 +1,18 @@
 using System.Windows;
 
+using ImeSense.SourceBase.Client.Views;
+
 namespace ImeSense.SourceBase.Client.ViewModels;
 
 public class MainViewModel : BaseViewModel {
     private RelayCommand? _exitMenuItemCommand;
     public RelayCommand ExitMenuItemCommand => _exitMenuItemCommand ??= new RelayCommand(_ => {
         Application.Current.Shutdown();
+    });
+
+    private RelayCommand? _showSettingsWindowCommand;
+    public RelayCommand ShowSettingsWindowCommand => _showSettingsWindowCommand ??= new RelayCommand(_ => {
+        var settingsWindow = new SettingsWindow();
+        settingsWindow.Show();
     });
 }
