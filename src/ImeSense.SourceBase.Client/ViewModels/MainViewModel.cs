351f3ffdda75f848139a815ad7dd9fde0e0404c1
acidicMercury8 <acidicmercury8@outlook.com>
2022-08-14 15:46:00 +0300

    Refactoring for `MainViewModel`

--- src/ImeSense.SourceBase.Client/ViewModels/MainViewModel.cs
+++ src/ImeSense.SourceBase.Client/ViewModels/MainViewModel.cs
@@ -5,18 +5,26 @@ using ImeSense.SourceBase.Client.Views;
 namespace ImeSense.SourceBase.Client.ViewModels;
 
 public class MainViewModel : BaseViewModel {
-    private RelayCommand? _exitMenuItemCommand;
-    public RelayCommand ExitMenuItemCommand => _exitMenuItemCommand ??= new RelayCommand(_ => {
+    private void ExitApplication(object? commandParameter) {
         Application.Current.Shutdown();
-    });
+    }
 
-    private RelayCommand? _showSettingsWindowCommand;
-    public RelayCommand ShowSettingsWindowCommand => _showSettingsWindowCommand ??= new RelayCommand(_ => {
+    private RelayCommand? _exitMenuItemCommand;
+    public RelayCommand ExitMenuItemCommand => _exitMenuItemCommand ??= new RelayCommand(ExitApplication);
+
+    private void ShowSettingsWindow(object? commandParameter) {
         var settingsWindow = new SettingsWindow();
         settingsWindow.Show();
-    });
-    private RelayCommand? _showAboutWindowCommand;
-    public RelayCommand ShowAboutWindowCommand => _showAboutWindowCommand ??= new RelayCommand(_ => {
+    }
+
+    private RelayCommand? _showSettingsWindowCommand;
+    public RelayCommand ShowSettingsWindowCommand => _showSettingsWindowCommand ??= new RelayCommand(ShowSettingsWindow);
+
+    private void ShowAboutWindow(object? commandParameter) {
         var aboutWindow = new AboutWindow();
         aboutWindow.Show();
-    });}
+    }
+
+    private RelayCommand? _showAboutWindowCommand;
+    public RelayCommand ShowAboutWindowCommand => _showAboutWindowCommand ??= new RelayCommand(ShowAboutWindow);
+}
